00dbce8d5e9ee46bd3bfce00e45a9151832dc565
André <andreamorimcouto22@gmail.com>
2020-08-17 21:27:07 +0100

    finished freecodecamp's solidity tutorial

---
+++ freecodecamp-solidity-tutorial/mycontract5.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.1;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    function mint() public{
+        balances[tx.origin] ++;
+    }
+}
+
+
+contract MyContract {
+    
+    address payable wallet;
+    address public token;
+    
+    constructor(address payable _wallet, address _token) public {
+        wallet= _wallet;
+        token= _token;
+    }
+    
+    function() external payable {
+        buyToken;
+    }
+    
+    function buyToken() public payable {
+        
+        ERC20Token(address(token)).mint();
+        
+        // send ether to the wallet
+        wallet.transfer(msg.value);
+        
+    }
+}
