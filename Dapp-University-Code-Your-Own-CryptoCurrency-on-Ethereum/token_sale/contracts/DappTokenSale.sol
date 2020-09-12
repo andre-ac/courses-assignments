11e144d5cc37752eddf9d01df8c554bf382d315f
André <andreamorimcouto22@gmail.com>
2020-09-12 23:54:05 +0100

    Continued Code Your Own Cryptocurrency on Eth

--- Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/contracts/DappTokenSale.sol
+++ Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/contracts/DappTokenSale.sol
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
 import "./DappToken.sol";
+
+contract DappTokenSale {
+        address admin;
+
+        constructor() {
+                admin = msg.sender;
+        }
+}
--- Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/migrations/2_deploy_contracts.js
+++ Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-const DappToken = artifacts.require("./DappToken.sol");
+var DappToken = artifacts.require("./DappToken.sol");
+var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappTokenSale);
 };
+
+
