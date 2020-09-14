77fd70d6ce1fcf18dcbfc997e67eb780c9318514
André <andreamorimcouto22@gmail.com>
2020-09-14 23:21:21 +0100

    Continued Code Your Own Cryptocurrency on Eth

--- Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/contracts/DappTokenSale.sol
+++ Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/contracts/DappTokenSale.sol
@@ -5,8 +5,12 @@ import "./DappToken.sol";
 
 contract DappTokenSale {
         address admin;
+        DappToken public tokenContract;
+        uint256 public tokenPrice;
 
-        constructor() {
+        constructor(DappToken _tokenContract, uint256 _tokenPrice) {
                 admin = msg.sender;
+                tokenContract = _tokenContract;
+                tokenPrice = _tokenPrice;
         }
 }
--- Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/test/DappTokenSale.js
+++ Dapp-University-Code-Your-Own-CryptoCurrency-on-Ethereum/token_sale/test/DappTokenSale.js
@@ -2,6 +2,7 @@ var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 contract('DappTokenSale', function(accounts){
         var tokenSaleInstance;
+        var tokenPrice = 1000000000000000
 
         it('initializes the contract with the correct values',function() {
                 return DappTokenSale.deployed().then(function(instance) {
@@ -9,9 +10,12 @@ contract('DappTokenSale', function(accounts){
                         return tokenSaleInstance.address;
                 }).then(function(address){
                         assert.notEqual(address, 0x0, 'has contract address');
-                        //return tokenSaleInstance.tokenContract();
-                });
+                        return tokenSaleInstance.tokenContract();
+                }).then(function(address){
+                        assert.notEqual(address, 0x0, 'has token contract address');
+                        return tokenSaleInstance.tokenPrice();
+                }).then(function(price){
+                        assert.equal(price, tokenPrice, 'token price is correct');
+                })
         });
-
-        }
-);
+});
